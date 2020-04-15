edee1e7017cf0fdece8caf6529e727836726c47d
Jean Dumont <jdumont@technicle.io>
2020-04-15 10:52:43 +0100

    Create serverdev.js

---
+++ server/serverdev.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+//var path = require('path');
+//var indexPath=__dirname + '\\..\\build\\web\\index.html'
+//console.log("INDEX :",path.join(indexPath))
+//console.log("WEB   :",path.join(__dirname, '../build/web'))
+
+
+app.get('/', (req, res) =>  res.sendFile("/work/dist/index.html"));
+app.use(express.static("/work/dist")))
+
+app.listen(port, () => console.log(`Technicle-home server listening on port ${port}`))
