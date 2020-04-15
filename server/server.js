87fb20d17ffcc8cf0c0e079390d000508d920a40
Jean Dumont <jdumont@technicle.io>
2020-04-15 10:49:27 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,13 +4,10 @@ const port = process.env.PORT || 8080
 
 var path = require('path');
 var indexPath=__dirname + '\\..\\build\\web\\index.html'
-console.log("indexPath",indexPath)
-//app.get('*', (req, res) =>  res.sendFile(path.join(indexPath)));
+console.log("INDEX :",path.join(indexPath))
+console.log("WEB   :",path.join(__dirname, '../build/web'))
+
+
 app.get('/', (req, res) =>  res.sendFile(path.join(indexPath)));
-app.get('/contact', (req, res) =>  res.sendFile(path.join(indexPath)));
-app.get('/terms', (req, res) =>  res.sendFile(path.join(indexPath)));
-app.get('/privacy', (req, res) =>  res.sendFile(path.join(indexPath)));
-app.get('/apps/plots/legal', (req, res) =>  res.sendFile(path.join(indexPath)));
-app.get('/apps/plots', (req, res) =>  res.sendFile(path.join(indexPath)));
 app.use(express.static(path.join(__dirname, '../build/web')))
 app.listen(port, () => console.log(`Technicle-home server listening on port ${port}`))
